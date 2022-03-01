2f51a04bafcc0e872cad3e552477b80cc3fed95e
Luke Nelson <luke@viderahealth.com>
2022-03-01 16:10:01 -0700

    New commit.

--- sol.py
+++ sol.py
@@ -6,7 +6,7 @@ def Newtons_Method(func, func_prime, x_0, iters=100, tol=1e-5):
              fuc_prime: its direvative
              x_0: an itial guess (required)
              iters:number of iterations to cap too (optional, default 100)
-             tol: acceptable distance from 0 (optional, default 1e-5)
+             tol: acceptable distance from 0 (optional, default 1e-5) 
      returns: a root(int) if found
               None(none-type) else
   """
@@ -14,4 +14,3 @@ def Newtons_Method(func, func_prime, x_0, iters=100, tol=1e-5):
 
 
 
- 
